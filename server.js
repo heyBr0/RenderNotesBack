728925b2995a5a73814eda3064e9e956a587d3ef
heyBr0 <benert@live.de>
2022-11-11 09:52:25 +0100

    Day 3: models & DB connect

--- server.js
+++ server.js
@@ -1,19 +1,27 @@
+const dotenv = require("dotenv");
+dotenv.config();
 const express = require("express");
 const app = express();
 const path = require("path");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const corsOptions = require("./config/config");
-const { logger } = require("./middleware/logger");
+const { logger, logEvents } = require("./middleware/logger");
 const errorHandler = require("./middleware/errorHandler");
+const mongoose = require("mongoose");
+const connectDB = require("./config/dbConn");
 //PORT
 const PORT = process.env.PORT || 3500;
+
+console.log(process.env.ENVtest);
+
+connectDB();
 
 //Middleware
 app.use(logger);
 app.use(cors(corsOptions));
-app.use(express.json())
-app.use(cookieParser())
+app.use(express.json());
+app.use(cookieParser());
 app.use("/", express.static(path.join(__dirname, "public")));
 app.use("/", require("./routes/root"));
 
@@ -30,6 +38,17 @@ app.all("*", (req, res) => {
 
 app.use(errorHandler);
 
-app.listen(PORT, () => {
-  console.log(`Server running on port: ${PORT}`);
+mongoose.connection.once("open", () => {
+  console.log("Connected to DB");
+  app.listen(PORT, () => {
+    console.log(`Server running on port: ${PORT}`);
+  });
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+  logEvents(
+    `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
+    "mongoErrLog.log"
+  );
 });
